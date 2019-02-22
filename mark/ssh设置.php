1db6d9303c1d5a37a120c03e59c5d50e68916dd0
e <wq33435063@126.com>
2019-02-22 13:58:05 +0800

    phpstudy 升级

--- mark/ssh设置.php
+++ mark/ssh设置.php
@@ -12,7 +12,7 @@
 git config --global user.name “用户名”
 
 git config --global user.email “邮箱”
-执行生成公钥和私钥的命令：ssh-keygen -t rsa 
+执行生成公钥和私钥的命令：ssh-keygen -t rsa -C ''
 并按回车3下（为什么按三下，是因为有提示你是否需要设置密码，如果设置了每次使用Git都会用到密码，一般都是直接不写为空，直接回车就好了）。
 会在一个文件夹里面生成一个私钥 id_rsa和一个公钥id_rsa.pub。
 执行查看公钥的命令：cat ~/.ssh/id_rsa.pub  
