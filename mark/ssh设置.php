95a9c4819859bc9b0fca40215b6188ad7eb3a15d
mpc <495285237@qq.com>
2020-03-25 16:00:37 +0800

    java签名

--- mark/ssh设置.php
+++ mark/ssh设置.php
@@ -17,4 +17,22 @@ git config --global user.email “邮箱”
 会在一个文件夹里面生成一个私钥 id_rsa和一个公钥id_rsa.pub。
 执行查看公钥的命令：cat ~/.ssh/id_rsa.pub  
 
+
+git init
+git remote add origin https://github.com/******/web // 第一次的时候需要
+git add .
+git commit -m "Initial commit"
+git push -u origin master // 第一次的时候需要 
+// 需要输入账号密码的时候，输入你在git申请的账号和设置过的密码就OK
+ 
+ 
+cd /Users/tuoge/Desktop/iOS/bluetoothLock  // 记得要cd到所有上传的文件目录下
+git status // 查看文件状态
+git add .
+git commit -m "修改" 
+git push // 以后就push一下就OK了
+
+————————————————
+版权声明：本文为CSDN博主「乔布斯狂热追随者」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/jwheat/article/details/80751740
 EOT;
